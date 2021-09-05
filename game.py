23794d45af66001757a4f6f5c8618863bcc741f3
Moosa Naeem <moosanaeem1@outlook.com>
2021-09-05 15:32:21 +1000

    added the burger and shaker a.i.

--- game.py
+++ game.py
@@ -1,1 +1,90 @@
-print("i am going to make the game.")
+import sys,pygame
+import random
+pygame.init()
+
+size = width, height = 1300, 1000
+
+screen = pygame.display.set_mode(size)
+black = 0,0,0
+
+
+def createProjectile(imagePath, initialX, initialY):
+    print("Created Projectile")
+    projectile = pygame.image.load(imagePath)
+    projectileControl = projectile.get_rect()
+    projectileControl.move_ip(initialX, initialY)
+
+
+
+
+burgerSpeed = [0,0]
+burger = pygame.image.load("./assets/burger.png")
+burgerControl = burger.get_rect()
+burgerControl.move_ip(400,800)
+
+
+powderSpeed = [0,0]
+powder = pygame.image.load("./assets/powder.png")
+powderControl = powder.get_rect()
+powderControl.move_ip(400,0)
+
+randomCounter = random.randint(0,200)
+counter = 0
+
+while 1:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            sys.exit()
+
+        if pygame.key.get_pressed()[pygame.K_RIGHT]:
+            	burgerSpeed[0] = 2
+            
+        if pygame.key.get_pressed()[pygame.K_LEFT]:
+            burgerSpeed[0] = -2
+
+        if pygame.key.get_pressed()[pygame.K_SPACE]:
+            createProjectile("./assets/sesameseed.png", burgerPositionX, burgerControl.y)
+
+
+    burgerControl = burgerControl.move(burgerSpeed)
+    powderControl = powderControl.move(powderSpeed)
+    
+    
+    #if burger collision then stop
+    if burgerControl.left < 0 or burgerControl.right > width:
+        burgerSpeed[0] = 0
+    #if powder collision then stop
+    if powderControl.left < 0 or powderControl.right > width:
+        powderSpeed[0] = 0
+
+
+
+    #POWDER AI v1
+    # if counter >= randomCounter:
+    #     powderSpeed[0] *= -1
+    #     counter = 0
+    #     randomCounter = random.randint(0,200)
+    # else:
+    #     counter += 1
+
+    #POWDER AI v2
+
+
+    burgerPositionX = burgerControl.x
+    powderPositionX = powderControl.x
+
+    if powderPositionX > burgerPositionX and counter >= randomCounter:
+        powderSpeed[0] = -1
+        counter = 0
+        randomCounter = random.randint(0,200)
+    elif powderPositionX <= burgerPositionX and counter >= randomCounter:
+        powderSpeed[0] = 1
+        counter = 0
+        randomCounter = random.randint(0,200)
+    else:
+        counter += 1
+
+    screen.fill(black)
+    screen.blit(burger, burgerControl)
+    screen.blit(powder, powderControl)
+    pygame.display.flip()
