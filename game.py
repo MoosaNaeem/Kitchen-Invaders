97fc23aad4b7748ac17f95206608809be5aa7095
Moosa Naeem <moosanaeem1@outlook.com>
2021-09-05 16:29:49 +1000

    made sesame seed projectile.

--- game.py
+++ game.py
@@ -18,18 +18,22 @@ def createProjectile(imagePath, initialX, initialY):
 
 
 burgerSpeed = [0,0]
-burger = pygame.image.load("./assets/burger.png")
+burger = pygame.image.load("./assets/burger.png").convert()
 burgerControl = burger.get_rect()
 burgerControl.move_ip(400,800)
 
 
 powderSpeed = [0,0]
-powder = pygame.image.load("./assets/powder.png")
+powder = pygame.image.load("./assets/powder.png").convert()
 powderControl = powder.get_rect()
 powderControl.move_ip(400,0)
 
 randomCounter = random.randint(0,200)
 counter = 0
+
+sesameSeeds = []
+seedboxes = []
+sesamePicture = pygame.image.load("./assets/sesameseed.png").convert()
 
 while 1:
     for event in pygame.event.get():
@@ -37,19 +41,32 @@ while 1:
             sys.exit()
 
         if pygame.key.get_pressed()[pygame.K_RIGHT]:
-            	burgerSpeed[0] = 2
+            burgerSpeed[0] = 2
             
         if pygame.key.get_pressed()[pygame.K_LEFT]:
             burgerSpeed[0] = -2
 
         if pygame.key.get_pressed()[pygame.K_SPACE]:
-            createProjectile("./assets/sesameseed.png", burgerPositionX, burgerControl.y)
+            #createProjectile("./assets/sesameseed.png", burgerPositionX, burgerControl.y)
 
+            # sesameSeeds.append([burgerControl.x,burgerControl.y])
+            seedbox = sesamePicture.get_rect()
+            seedbox.move_ip(burgerControl.x, burgerControl.y)
+            seedboxes.append(seedbox)
+        pygame.time.Clock().tick(60)
 
     burgerControl = burgerControl.move(burgerSpeed)
     powderControl = powderControl.move(powderSpeed)
     
     
+    for seed in seedboxes:
+       seed.y += -5
+
+    # Iterate over a slice copy if you want to mutate a list.
+    # for sesame in seedboxes[:]:
+    #     if sesame[0] < 0:
+    #         seedboxes.remove(sesame)
+            
     #if burger collision then stop
     if burgerControl.left < 0 or burgerControl.right > width:
         burgerSpeed[0] = 0
@@ -87,4 +104,6 @@ while 1:
     screen.fill(black)
     screen.blit(burger, burgerControl)
     screen.blit(powder, powderControl)
+    for sesame in seedboxes:
+        screen.blit(sesamePicture, sesame)
     pygame.display.flip()
